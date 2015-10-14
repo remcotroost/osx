040c24281c6a6468e770ea81d91edb902da5d310
Brooke Kuhlmann <brooke@alchemists.io>
2015-10-14 09:48:19 -0600

    Refactored Homebrew install to use long options.
    - Makes it easier to read/debug what the install is doing.

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -6,7 +6,7 @@
 # EXECUTION
 # Homebrew
 if ! command -v brew > /dev/null; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  ruby -e "$(curl --location --fail --silent --show-error https://raw.githubusercontent.com/Homebrew/install/master/install)"
   export PATH="/usr/local/bin:$PATH"
   printf "export PATH=\"/usr/local/bin:$PATH\"\n" >> $HOME/.bash_profile
 fi
