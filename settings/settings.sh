54b2ea47a563050d7ca3ca3d377c45b7aad0364e
Brooke Kuhlmann <brooke@alchemists.io>
2016-02-03 21:28:45 -0700

    Updated to Trailer 1.3.16.

--- settings/settings.sh
+++ settings/settings.sh
@@ -226,7 +226,7 @@ export HAZEL_APP_NAME=Hazel.prefPane
 export HAZEL_APP_URL="http://www.noodlesoft.com/Products/Hazel/download"
 
 export TRAILER_APP_NAME=Trailer.app
-export TRAILER_APP_URL="http://ptsochantaris.github.io/trailer/trailer1311.zip"
+export TRAILER_APP_URL="http://ptsochantaris.github.io/trailer/trailer1316.zip"
 
 export CARBON_COPY_CLONER_APP_NAME="Carbon Copy Cloner.app"
 export CARBON_COPY_CLONER_APP_URL="https://70dd08a0984b0022574d-8064f6cbda3d6f2abd1c53b8bc16b17e.ssl.cf5.rackcdn.com/ccc-4.1.6.4255.zip"
