b588405a32954dcd65a705ca9594f18e6d5aebdc
Brooke Kuhlmann <brooke@alchemists.io>
2016-03-21 19:38:55 -0600

    Updated to AppCleaner 3.3.

--- settings/settings.sh
+++ settings/settings.sh
@@ -210,7 +210,7 @@ export SONOS_APP_NAME=Sonos.app
 export SONOS_APP_URL="http://www.sonos.com/redir/controller_software_mac"
 
 export APP_CLEANER_APP_NAME=AppCleaner.app
-export APP_CLEANER_APP_URL="https://freemacsoft.net/downloads/AppCleaner_3.2.1.zip"
+export APP_CLEANER_APP_URL="https://freemacsoft.net/downloads/AppCleaner_3.3.zip"
 
 export HAZEL_APP_NAME=Hazel.prefPane
 export HAZEL_APP_URL="http://www.noodlesoft.com/Products/Hazel/download"
